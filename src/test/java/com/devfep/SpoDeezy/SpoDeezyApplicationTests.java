b43b14794d5c800d6b213b14827e409421dcb64c
Felix P <devfep@gmail.com>
2022-12-11 22:34:15 -0500

    Add front-end

---
+++ src/test/java/com/devfep/SpoDeezy/SpoDeezyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfep.SpoDeezy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoDeezyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
